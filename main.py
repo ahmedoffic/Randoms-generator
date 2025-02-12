86cbf5b287111d60df79f6053cc7ec04ec3cdf98
Ahmed Mo. Official <afterallidid3210@gmail.com>
2025-02-12 17:47:03 +0200

    Generate first attempt of random number v0.0.001
    Making more changes add Rams SYS and CPU and Disk functions and class
    edit simple random generating to generate simple numbers

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from utils import random
 randomValue = random.Simple()
 def run():
     print("manipulating")
-    print("My random number: ",randomValue.Generate)
+    # print("My random number: ",randomValue.value)
+    values = [random.Simple().value for _ in range(1000)]
+    print("Generated values:", values)
+    # print(randomValue._eq)
 if __name__ == "__main__":
     run()
--- utils/random.py
+++ utils/random.py
@@ -1,8 +1,16 @@
 from . import hardware
 class Simple:
-    def __ini__(self):pass
-    def mouse_inputs(self):return hardware.Mouse.location()
-    def cpu_temp(self):return hardware.Cpu.temp()
+    def __init__(self,GEN=True,Range=range(0,1)):
+        self.GEN = GEN
+        self.Range = Range
+        if self.GEN:self.value = self.Generate()
+    def mouse_inputs(self):return hardware.Mouse().location()
+    # def cpu_temp(self):return hardware.Cpu.temp()
+    def type(self):
+        print("TYPE: hardware.SYS().disckIOCounters() ",type(hardware.SYS().disckIOCounters()))
     def Generate(self):
-        return self.mouse_inputs + 1
-    
+        self.seed = hardware.SYS().disckIOCounters()
+        self.x = hardware.SYS().time() / ((1/3)*(hardware.SYS().time()))
+        self.y = hardware.Cpu().sysCalls()
+        self._eq = self.seed * ((self.x / self.mouse_inputs() )*self.y)*hardware.Cpu().count()
+        return self._eq
