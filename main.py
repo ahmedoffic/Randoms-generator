51968bb6bbf6c07e73200391cdebe709d0731d2c
Ahmed Mo. Official <afterallidid3210@gmail.com>
2025-02-25 15:49:44 +0200

    Posting some edits

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from utils import random
 randomValue = random.Simple()
+
 def run():
     print("manipulating")
     print("Generated values:", randomValue.value)
+
 if __name__ == "__main__":
     run()
